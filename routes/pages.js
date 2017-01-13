f125c824509ec50de6b7cffe514451b162f15e1c
Durling <878877528@qq.com>
2017-01-13 14:28:08 +0800

    更新

--- routes/pages.js
+++ routes/pages.js
@@ -17,56 +17,20 @@ var connection = mysql.createConnection({
 
 // 默认页
 router.get('/',function (req,res,next){
-  res.redirect('poems');
+  var renderData = {
+    title: "诗友圈"
+  };
+  res.render('index/pc', renderData);
 });
 
 // 诗列表、详情
 router.get('/poems',function (req,res,next){
 
-	var act = req.query.act;
-	console.log('act===>'+act);
-	var act_status;
-	if (req.query.id>0) {
-		var query = 'select * from poems where id='+req.query.id+';';
-		connection.query(query,function(error,rows,fields){
+  var renderData = {
+    title: "诗友圈"
+  };
+  res.render('index/pc', renderData);
 
-			if(act=='edit'){
-				act_status = 1; //编辑
-				title = '编辑';
-			}else{
-				act_status = 0; //详情
-				title = '诗印象'; 
-			}
-			var renderData = {
-				title:title,
-				rows:rows,
-				act_status:act_status
-			};
-			// console.log(renderData);
-			res.render('poems/poems',renderData);
-		})
-	}else{
-		var query = 'select id,title,authorId,authorName from poems;';
-		connection.query(query,function(error,rows,fields){
-			if(act=='add'){
-				act_status = 2; //新建 
-				title = '写个诗';
-			}else if(act=='correction'){
-				act_status = 3;
-				title = '纠错'; //纠错
-			}else{
-				act_status = -1;
-				title = '诗列表'; // 列表
-			}
-			var renderData = {
-				title:title,
-				rows:rows,
-				act_status:act_status
-			};
-			console.log('renderData===>'+renderData);
-			res.render('poems/poems',renderData);
-		})
-	}
 })
 
 
